7b431ab606c45f8f60aa046d9fc4245658adcef9
aayush2910 <fularaaayush2910@gmail.com>
2022-12-18 16:12:36 +0530

    while loops

---
+++ while loop.c
@@ -0,0 +1,68 @@
+/*program for natural no. from 1 to n
+#include<stdio.h>
+int main (){
+    int i,end;
+    printf("enter all no. from 1 to :");
+    scanf("%d",&end);
+    i=1;
+    while(i<=end){
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}
+//natural no. in reverse (from n to 1)
+#include <stdio.h>
+int main(){
+    int n;
+    printf("enter the natural number n :");
+    scanf("%d",&n);
+    while(n>=1){
+        printf("%d ",n);
+        --n;
+    }
+    return 0;
+}
+//alphabet from a to z
+#include<stdio.h>
+int main(){
+    char ch = 'A';
+    while (ch<='Z')
+    {
+        printf("%c ",ch);
+        ch++;
+    }
+    return 0;
+}
+//even natural no. from 1 to 100
+#include<stdio.h>
+int main(){
+    int i,n;
+    printf("enter the natural no. till :");
+    scanf("%d",&n);
+    i=1;
+    while (i<=n)
+    {
+        if(i%2== 0){
+            printf("%d\n",i);
+        }
+        i++;
+    }
+    return 0;
+}*/
+//odd natural no. from 1 to 100
+#include<stdio.h>
+int main(){
+    int i,n;
+    printf("enter the natural no. till :");
+    scanf("%d",&n);
+    i=1;
+    while (i<=n)
+    {
+        if(i%2== 1){
+            printf("%d\n",i);
+        }
+        i++;
+    }
+    return 0;
+}
